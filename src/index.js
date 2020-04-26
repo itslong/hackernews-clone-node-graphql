b794dafaa81083ce08fdb154a8f97691ba37a47f
Long Liang <lngr23@gmail.com>
2020-04-26 11:16:52 -0700

    added a Link type and resolvers.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
 type Query {
   info: String!
-}`;
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`;
+
+// dummy data
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.link0.com',
+    description: 'some link'
+  },
+  {
+    id: 'link-1',
+    url: 'www.anotherlink.com',
+    description: 'more links'
+  }
+];
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 };
 
